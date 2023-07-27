a034ea46d9076bb6b598b6dfeefa2bd8bc842606
DIRSON <dgutsal221@gmail.com>
2023-07-27 22:06:44 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 255;
+            short b = -99;
+            int c = -239467229;
+            float d = 65.46f;
+            string e = "World";
+            char f = 'a';
+        }
+    }
+}
